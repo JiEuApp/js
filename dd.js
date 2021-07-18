d23bfb2e1690f55c1099349ee361ee4e4d39c1dc
App <83689802+nahgs@users.noreply.github.com>
2021-07-18 17:23:40 +0800

    Update dd.js

--- dd.js
+++ dd.js
@@ -88,7 +88,11 @@
 				}
 				if (parseInt(sjh) > 0 && parseInt(yqm) > 0 && parseInt(sjh) > 13000000000 && parseInt(sjh) < 19999999999 && parseInt(yqm) > 100000 && parseInt(yqm) < 999999) {
 					huoqu(sjh, yqm);
-					mui.showLoading('正在登录', 'div')
+					
+                    			api.showProgress({
+						text: '正在登录',
+						
+					});
 					
 
 				}
